0a5df27e0f9b46a8bfc77c9431ad9ac6bdd54ca8
Roe Greene <robert@byrojo.com>
2022-05-07 23:54:56 -0600

    add category grid

--- components/home-page/index.tsx
+++ components/home-page/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Hero from '../hero'
 import RecentlyAddedSection from './components/recently-added-section'
-import CategoryGrid from './components/category-grid'
+import FancyCategoryGrid from './components/fancy-category-grid'
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Hero>
-        <CategoryGrid />
+        <FancyCategoryGrid />
       </Hero>
       <RecentlyAddedSection />
     </>
